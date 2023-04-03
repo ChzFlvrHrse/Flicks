4975202ef7f3051173d0734bce6f47b204203748
Nathan <nscott1010@gmail.com>
2023-04-03 13:37:06 -0500

    home page component

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -2,23 +2,18 @@ import React, { useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { getAllFlicksThunk, getOneFlickThunk, searchFlickThunk } from '../store/flick'
+import { getAllCategoriesThunk, getOneCategoryThunk } from '../store/category';
 
 export default function Category() {
     const dispatch = useDispatch()
 
-    // useEffect(() => {
-    //     dispatch(getAllFlicksThunk())
-    // }, [dispatch])
 
-    useEffect(() => {
-        dispatch(getOneFlickThunk('zebra'))
-    }, [dispatch])
 
     return (
-        <div>
+        <>
             <div>
                 Test
             </div>
-        </div>
+        </>
     )
 }
