25968687f6a9fc3ffcbe1720fe579c79eb18d53b
JB Kam <jb3.kam@gmail.com>
2023-04-03 13:40:40 -0700

    updated

--- react-app/src/components/Slick/slider.js
+++ react-app/src/components/Slick/slider.js
@@ -3,15 +3,16 @@ import Slider from "react-slick";
 import { SampleNextArrow, SamplePrevArrow } from './arrowFunctions';
 import "./slick-theme.css"
 import "./slick.css"
+import "./index.css"
 
 export default function SliderComponent() {
     var settings = {
         // className: "center",
         // centerMode: true,
+        // centerPadding: "70px",
         infinite: true,
-        centerPadding: "60px",
-        slidesToShow: 5,
-        slidesToScroll: 5,
+        slidesToShow: 6,
+        slidesToScroll: 6,
         speed: 500,
         dots: true,
         nextArrow: <SampleNextArrow />,
@@ -20,41 +21,42 @@ export default function SliderComponent() {
 
     return (
         <div className='slider-container'>
+            <h2>Trending Now</h2>
             <Slider {...settings}>
-                <div>
+                <div className="content">
                     <h3>1</h3>
                 </div>
-                <div>
+                <div className="content">
                     <h3>2</h3>
                 </div>
-                <div>
+                <div className="content">
                     <h3>3</h3>
                 </div>
-                <div>
+                <div className="content">
                     <h3>4</h3>
                 </div>
-                <div>
+                <div className="content">
                     <h3>5</h3>
                 </div>
-                <div>
+                <div className="content">
                     <h3>6</h3>
                 </div>
-                <div>
+                <div className="content">
                     <h3>1</h3>
                 </div>
-                <div>
+                <div className="content">
                     <h3>2</h3>
                 </div>
-                <div>
+                <div className="content">
                     <h3>3</h3>
                 </div>
-                <div>
+                <div className="content">
                     <h3>4</h3>
                 </div>
-                <div>
+                <div className="content">
                     <h3>5</h3>
                 </div>
-                <div>
+                <div className="content">
                     <h3>6</h3>
                 </div>
             </Slider>
--- react-app/src/components/mainPage.js
+++ react-app/src/components/mainPage.js
@@ -2,13 +2,19 @@ import React, { useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
 import { getAllFlicksThunk, getOneFlickThunk, searchFlickThunk } from '../store/flick'
-import "./Slick/slick-theme.css"
-import "./Slick/slick.css"
+import { getAllCategoriesThunk, getOneCategoryThunk } from '../store/category';
 import "../index.css"
 import SliderComponent from './Slick/slider';
 
 export default function Category() {
   const dispatch = useDispatch()
+
+  // const allCagegories = useSelector(state => Object.values(state.Category))
+  // console.log(allCagegories)
+  useEffect(() => {
+    dispatch(getAllCategoriesThunk())
+  }, [dispatch])
+
 
   return (
     <div>
